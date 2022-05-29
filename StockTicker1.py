4ca5145994a33474960a9ba5ee92f0f06eaf8476
codingwithnitesh <91451667+codingwithnitesh@users.noreply.github.com>
2022-05-29 20:43:17 +1000

    Update StockTicker1.py

--- StockTicker1.py
+++ StockTicker1.py
@@ -8,6 +8,7 @@
 # Test changes Nitesh 7:28
 # DEVELOP THE GRAPH LAYOUT FIRST, AND LEAVE THE CALLBACK FOR THE NEXT PHASE
 # comment on 9th May
+# let's check this
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
